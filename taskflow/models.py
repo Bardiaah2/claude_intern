dcc3eb5fdf250bf0ca584e94800504589d700218
Bardia <bardiaah@uorgeon.edu>
2026-08-17 23:21:58 -0700

    fix2 TF-103:fixed class level variables

--- taskflow/models.py
+++ taskflow/models.py
@@ -1,32 +1,21 @@
 from dataclasses import dataclass, field
 from datetime import datetime
-# from enum import Enum
-
-# class Priority(Enum):
-#     LOW = "low"
-#     MEDIUM = "medium"
-#     HIGH = "high"
-
 
 @dataclass
 class Task:
     """A single task tracked by TaskManager.  
     Priorities: low, medium, high"""
-    __PRIORITIES = {"low", "medium", "high"}
+    _PRIORITIES = {"low", "medium", "high"}
     id: int
     title: str
     priority: str = "medium"
+    completed: bool = False
+    created_at: datetime = field(default_factory=datetime.now)
     
 
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-        self.completed: bool = False
-        self.created_at: datetime = field(default_factory=datetime.now)
-
+    def __post_init__(self):
         self.priority = self.priority.lower().strip()
-        # if self.priority in Priority._value2member_map_.keys():
-        #     self.priority = Priority._value2member_map_[self.priority]
-        if self.priority in self.__PRIORITIES:
+        if self.priority in self._PRIORITIES:
             pass
         else:
             raise ValueError("Task can only take low, medium and high as priorit.")
